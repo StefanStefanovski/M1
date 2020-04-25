9ca0d167752c654bd4a4385b424ba0846f1117a6
stefanovski <stefanovski_stefan@hotmail.com>
2020-04-25 03:33:29 +0200

    exemples de formules (1) definition de boolean (2.1)

--- S2/hmin229/Projet/projet.v
+++ S2/hmin229/Projet/projet.v
@@ -1,10 +1,49 @@
-Section Ensembles.
-  Variable U : Type.
+Parameters A B C: Prop.
+(* 1.1 ==>  **** Exemples de formules ****)
 
-  Definition Ensemble := U -> Prop.
+Lemma ex1: B -> A \/ B.
+Proof.
+intro.
+right.
+assumption.
+Qed.
 
-  Definition In (V:Ensemble) (A:U) : Prop := V A.
+Lemma ex2: A \/ B -> B \/ A.
+Proof.
+(*tauto.*)
+intro.
+elim H.
+intro.
+right.
+assumption.
+intro.
+left.
+assumption.
+Qed.
 
-  Definition Included (V F:Ensemble) : Prop := forall A:U, In V A -> In F A.
+(* 2.1 ==> **** Définition de la notion de satisfiabilité, validité, insatisfiabilité*)
+(* On definit le boolean (bool) pour les valeurs 
+de variables de propositions*)
 
-  Inductive Empty_set : Ensemble :=.
+Inductive bool : Set :=
+  | true : bool
+  | false : bool.
+
+(* On definit le la fonction et (/\) B x B vers B (B: bool) *)
+ 
+
+
+
+Definition andB (b1 b2 : bool) : bool :=
+  match b1, b2 with
+    | true, true => true
+    | true, false => false
+    | false, true => false
+    | false, false => false
+  end.
+Lemma exempleAndBool: forall a b: bool, andB a b = true -> a = true /\ b = true.
+Proof.
+intros.
+elim H.
+split.
+
